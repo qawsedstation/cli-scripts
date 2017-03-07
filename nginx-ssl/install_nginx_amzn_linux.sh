a8e1b3538134920cdd655eaf703d7dfda4cc2e3f
jake-gg <jake@giffgaffMjake.local>
2017-03-07 15:20:10 +0000

    script to install nginx and ssl on AmznLinux

---
+++ nginx-ssl/install_nginx_amzn_linux.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# INSTALL NGINX - https://www.digitalocean.com/community/tutorials/how-to-create-a-ssl-certificate-on-nginx-for-centos-6
+# enabled epel repo
+sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/epel.repo
+yum -y install nginx
+sudo mkdir /etc/nginx/ssl
+cd /etc/nginx/ssl
+
+# Generate cert and CSR
+# https://www.jamescoyle.net/how-to/1073-bash-script-to-create-an-ssl-certificate-key-and-request-csr
+
+# Required
+domain=`hostname`
+ 
+# Change to your company details
+commonname=$domain
+country=GB
+state=London
+locality=London
+organization=example
+organizationalunit=Infrastructure
+email=pki@example.com
+ 
+# Optional
+password=TemporaryPassword
+ 
+echo "Generating key request for $domain"
+# Generate a key
+openssl genrsa -des3 -passout pass:$password -out $domain.key 2048 -noout
+ 
+# Remove passphrase from the key. Comment the line out to keep the passphrase
+echo "Removing passphrase from key"
+openssl rsa -in $domain.key -passin pass:$password -out $domain.key
+ 
+# Create the request
+echo "Creating CSR"
+openssl req -new -key $domain.key -out $domain.csr -passin pass:$password \
+    -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+
+# Signing the request
+sudo openssl x509 -req -days 365 -in $domain.csr -signkey $domain.key -out $domain.crt
+
+# HTTPS server
+echo -e "\nserver {\n    listen       443;\n    server_name $domain;\n    ssl on;\n    ssl_certificate /etc/nginx/ssl/$domain.crt;\n    ssl_certificate_key /etc/nginx/ssl/$domain.key; \n}" >> /etc/nginx/conf.d/virtual.conf
+/etc/init.d/nginx restart
